5d81cb3f8ec3c334174eeb88db32a59ea66946ba
Simon Hoefling <simon@hoeflia.eu>
2023-08-20 14:46:46 +0800

    Added possibility to add a book
    It's not finished I still have to implement to fetch a cover for a new book from Oben Librarys Cover API

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,13 +1,13 @@
 // src/components/BookList.tsx
 import React, { useEffect, useState } from 'react';
-import { fetchBooks, updateBook, deleteBook } from '../services/api'; // Update import to include deleteBook
+import { fetchBooks, updateBook, deleteBook, createBook } from '../services/api'; // Update import to include createBook
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBook } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 import './BookList.css';
 
 // Define the Book interface
@@ -35,6 +35,12 @@ const BookList = () => {
   const [selectedFirstPublishYear, setSelectedFirstPublishYear] = useState<number>(0);
   const [selectedNumberOfPages, setSelectedNumberOfPages] = useState<number>(0);
   const [bookToDelete, setBookToDelete] = useState<Book | null>(null);
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author_name: '',
+    first_publish_year: 0,
+    number_of_pages_median: 0,
+  });
 
   // Load books from API on sorted by title
   useEffect(() => {
@@ -86,32 +92,55 @@ const BookList = () => {
     }
   };
 
+  // Function to add a new book
+  const handleAddBook = async () => {
+    // Replace with your API call to create a new book
+    const response = await createBook(newBook);
+
+    // Update the books list with the new book
+    setBooks([...books, response.data]);
+
+    // Close the modal
+    handleCloseModal();
+  };
 
   return (
     <Container>
-      <h4 className='text-secondary mb-4'><FontAwesomeIcon icon={faBook} /> Add book</h4>
+      <h4
+        className="text-secondary mb-4"
+        onClick={() => {
+          // Clear previous new book data (is needed when adding a new )
+          setNewBook({
+            title: '',
+            author_name: '',
+            first_publish_year: 0,
+            number_of_pages_median: 0,
+          });
+          // Open the modal for adding a new book
+          setShowModal(true);
+        }}
+        style={{ cursor: 'pointer' }}
+      >
+        <FontAwesomeIcon icon={faBook} /> Add book
+      </h4>
       {/* Display the list of books */}
       <div className="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 mb-5">
         {books.map((book) => (
           <div key={book.id} className="col mb-4">
             <Card className="h-100 shadow">
-              <div
-                style={{
-                  position: 'relative',
-                  paddingBottom: '100%',
-                  overflow: 'hidden',
-                }}
-              >
-                <Card.Img
-                  variant="top"
-                  src={book.covers.M}
-                  alt={book.title}
-                  style={{
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                  }}
-                />
+              <div style={{
+                position: 'relative',
+                paddingBottom: '100%',
+                overflow: 'hidden'
+                }}>
+                {book.covers && book.covers.M ? (
+                  <Card.Img
+                    variant="top"
+                    src={book.covers.M}
+                    alt={book.title}
+                    style={{ position: 'absolute', width: '100%', height: '100%' }}
+                  />
+                ) : null}
               </div>
               <Card.Body className="d-flex flex-column">
                 <Card.Title>{book.title}</Card.Title>
@@ -142,21 +171,25 @@ const BookList = () => {
           </div>
         ))}
       </div>
-      {/* Modal for editing a book */}
+      {/* Modal for editing or adding a book */}
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Edit Book</Modal.Title>
+          <Modal.Title>{selectedBook ? 'Edit Book' : 'Add Book'}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {/* Form for editing book details */}
+          {/* Form for editing or adding book details */}
           <Form>
             <Form.Group controlId="formTitle">
               <Form.Label>Title</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter title"
-                value={editedTitle}
-                onChange={(e) => setEditedTitle(e.target.value)}
+                value={selectedBook ? editedTitle : newBook.title}
+                onChange={(e) =>
+                  selectedBook
+                    ? setEditedTitle(e.target.value)
+                    : setNewBook({ ...newBook, title: e.target.value })
+                }
               />
             </Form.Group>
             <Form.Group controlId="formAuthor">
@@ -164,8 +197,12 @@ const BookList = () => {
               <Form.Control
                 type="text"
                 placeholder="Enter author"
-                value={editedAuthor}
-                onChange={(e) => setEditedAuthor(e.target.value)}
+                value={selectedBook ? editedAuthor : newBook.author_name}
+                onChange={(e) =>
+                  selectedBook
+                    ? setEditedAuthor(e.target.value)
+                    : setNewBook({ ...newBook, author_name: e.target.value })
+                }
               />
             </Form.Group>
             <Form.Group controlId="formFirstPublishYear">
@@ -173,8 +210,17 @@ const BookList = () => {
               <Form.Control
                 type="number"
                 placeholder="Enter first published year"
-                value={selectedFirstPublishYear}
-                onChange={(e) => setSelectedFirstPublishYear(parseInt(e.target.value))}
+                value={
+                  selectedBook ? selectedFirstPublishYear : newBook.first_publish_year
+                }
+                onChange={(e) =>
+                  selectedBook
+                    ? setSelectedFirstPublishYear(parseInt(e.target.value))
+                    : setNewBook({
+                        ...newBook,
+                        first_publish_year: parseInt(e.target.value),
+                      })
+                }
               />
             </Form.Group>
             <Form.Group controlId="formNumPagesMedian">
@@ -182,8 +228,17 @@ const BookList = () => {
               <Form.Control
                 type="number"
                 placeholder="Enter number of pages median"
-                value={selectedNumberOfPages}
-                onChange={(e) => setSelectedNumberOfPages(parseInt(e.target.value))}
+                value={
+                  selectedBook ? selectedNumberOfPages : newBook.number_of_pages_median
+                }
+                onChange={(e) =>
+                  selectedBook
+                    ? setSelectedNumberOfPages(parseInt(e.target.value))
+                    : setNewBook({
+                        ...newBook,
+                        number_of_pages_median: parseInt(e.target.value),
+                      })
+                }
               />
             </Form.Group>
           </Form>
@@ -192,8 +247,11 @@ const BookList = () => {
           <Button variant="secondary" onClick={handleCloseModal}>
             CLOSE
           </Button>
-          <Button variant="success" onClick={handleSaveChanges}>
-            SAVE CHANGES
+          <Button
+            variant={selectedBook ? 'success' : 'primary'}
+            onClick={selectedBook ? handleSaveChanges : handleAddBook}
+          >
+            {selectedBook ? 'SAVE CHANGES' : 'ADD BOOK'}
           </Button>
         </Modal.Footer>
       </Modal>
@@ -225,7 +283,6 @@ const BookList = () => {
           </Button>
         </Modal.Footer>
       </Modal>
-
     </Container>
   );
 };
